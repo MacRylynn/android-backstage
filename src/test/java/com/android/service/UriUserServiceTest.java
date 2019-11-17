e0f057fa314032a8622b4aed2cdf37bf7fc52257
530454062@qq.com <vacuum158013>
2019-11-17 21:38:25 +0800

    modify:run

--- src/test/java/com/android/service/UriUserServiceTest.java
+++ src/test/java/com/android/service/UriUserServiceTest.java
@@ -1,12 +1,12 @@
-package com.android.service;
-
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class UriUserServiceTest {
-
-}
+//package com.android.service;
+//
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class UriUserServiceTest {
+//
+//}
