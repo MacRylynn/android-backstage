65432d581aeafa6078897b4c5ac5927a61c18028
Macrylynn <530454062@qq.com>
2019-11-25 14:46:52 +0800

    add:okhttp3

--- src/test/java/com/android/service/OkhttpTest.java
+++ src/test/java/com/android/service/OkhttpTest.java
@@ -4,6 +4,7 @@ package com.android.service;
 import com.alibaba.fastjson.JSON;
 import com.android.domain.base.CommonRequest;
 import com.android.domain.request.UriAccountInfoReq;
+import com.android.domain.request.UriDownLoadReq;
 import okhttp3.*;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -11,7 +12,9 @@ import org.slf4j.LoggerFactory;
 
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 public class OkhttpTest {
@@ -107,45 +110,71 @@ public class OkhttpTest {
     //3. 下载文件
     @Test
     public void DownloadInOkHttp() {
+        String url = "http://127.0.0.1:8080/web/check/downloadfile";
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+
+        CommonRequest<UriDownLoadReq> commonRequest = new CommonRequest<UriDownLoadReq>();
+        UriDownLoadReq uriDownLoadReq = new UriDownLoadReq();
+        uriDownLoadReq.setId(15L);
+        commonRequest.setRequestData(uriDownLoadReq);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(mediaType, JSON.toJSONString(commonRequest)))
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                writeFile(response);//保存文件
+                System.out.println(response.protocol() + " " + response.code() + " " + response.message());
+            }
+        });
+    }
+
+    //保存文件
+    private static void writeFile(Response response) {
+        InputStream is = null;
+        byte[] buf = new byte[2048];
+        int len = 0;
+        FileOutputStream fos = null;
+        String fileName = "result.png";
+        //String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String path = "C:/Users/lijiao73/Desktop/aim";
+        //储存下载文件的目录
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File file = new File(dir, fileName);
+        try {
+            is = response.body().byteStream();
+            fos = new FileOutputStream(file);
+            long sum = 0;
+            while ((len = is.read(buf)) != -1) {
+                fos.write(buf, 0, len);
+                sum += len;
+            }
+            fos.flush();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+
+            }
+
+        }
 
     }
-//    //保存文件
-//    private void writeFile(Response response) {
-//        InputStream is = null;
-//        FileOutputStream fos = null;
-//        is = response.body().byteStream();
-//        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-//        File file = new File(path, fileName);
-//        try {
-//            fos = new FileOutputStream(file);
-//            byte[] bytes = new byte[1024];
-//            int len = 0;
-//            //获取下载的文件的大小
-//            long fileSize = response.body().contentLength();
-//            long sum = 0;
-//            int porSize = 0;
-//            while ((len = is.read(bytes)) != -1) {
-//                fos.write(bytes);
-//                sum += len;
-//                porSize = (int) ((sum * 1.0f / fileSize) * 100);
-//                Message message = handler.obtainMessage(1);
-//                message.arg1 = porSize;
-//                handler.sendMessage(message);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (is != null) {
-//                    is.close();
-//                }
-//                if (fos != null) {
-//                    fos.close();
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        Log.i("myTag", "下载成功");
-//    }
+
 }
