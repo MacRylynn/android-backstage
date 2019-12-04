156283ce2343e2cda1b15647cd5d7814e5338402
530454062@qq.com <vacuum158013>
2019-12-04 15:58:19 +0800

    modify:opencv test

--- src/main/java/com/android/common/util/ImageProcess.java
+++ src/main/java/com/android/common/util/ImageProcess.java
@@ -1,5 +1,7 @@
 package com.android.common.util;
 
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
@@ -11,6 +13,16 @@ import java.io.Serializable;
  * @Date: 2019/11/13 17:11
  */
 public class ImageProcess implements Serializable {
+
+//    public static void main(String[] args) {
+//        //加载 opencv
+//        System.load("H:\\软件压缩包\\OpenCV for Java\\OpenCV_3_4_7\\build\\java\\x64\\opencv_java347.dll");
+//        Mat src = Imgcodecs.imread("C:\\Users\\Administrator\\Desktop\\results\\4.jpg");
+//        int[] result = ProcessResults.getlevel(src);
+//        for (int a :result){
+//            System.out.print(a+" ");
+//        }
+//    }
 
     // todo 调用算法处理
     public static String imageProcess(MultipartFile file) {
